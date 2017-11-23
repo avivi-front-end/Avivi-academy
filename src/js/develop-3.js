6be065402d57756abc00c13f7f2ee30c52ed1fdc
igor-str <igorok.strilchuk@gmail.com>
2017-11-23 17:14:49 +0200

    add slider in blog-post

--- src/js/develop-3.js
+++ src/js/develop-3.js
@@ -1,3 +1,25 @@
-/**
- * Created by PM on 17.11.2017.
- */
+$('.slick-target').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 680,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '<div class="slider-left"><svg id="slider-left" viewBox="0 0 477.175 477.175" width="20px" height="31px" ><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225   c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></div>',
+                nextArrow: '<div class="slider-right"><svg id="slider-right" viewBox="0 0 477.175 477.175" width="20px" height="31px" ><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5   c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></svg></div>'
+            }
+        }
+    ]
+});
