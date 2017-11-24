b1fdbe16cba146d7f60315c25b9cf213c071426e
tampledog <tampledog@gmail.com>
2017-11-24 12:03:59 +0200

    sliders

--- src/js/develop.js
+++ src/js/develop.js
@@ -175,6 +175,42 @@ $(document).ready(function () {
         ]
     });
 
+    $('.white-string__sections').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
 
 
 });
